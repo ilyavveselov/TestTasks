d81fa92618079ad8e7a0ac35960cf521cb514a29
Ilya Veselov <ilyaveselov05@yandex.ru>
2025-05-27 21:00:38 +0400

    Добавил зависимость репозитория в стартап файл

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,7 +1,10 @@
+using Task2.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 
 var app = builder.Build();
 
