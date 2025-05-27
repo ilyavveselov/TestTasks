3e91975c61cd0bca37f77966230a4da449de973e
Ilya Veselov <ilyaveselov05@yandex.ru>
2025-05-27 20:56:55 +0400

    Добавил модель для пицц, добавил папку с изображениями для верстки пицц

---
+++ Task2/Models/PizzaModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task2.Models
+{
+    public class PizzaModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        public List<int> Sizes { get; set; } = new();
+        public List<string> Types { get; set; } = new();
+
+        [Required]
+        public int Price { get; set; }
+        public int Weight { get; set; }
+        public bool ShowHalf { get; set; }
+        public bool CanHalf { get; set; }
+        public bool IsHit { get; set; }
+    }
+}
