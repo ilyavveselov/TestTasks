c220f974e9254eb0687fcf18327b859e00cfefff
Ilya Veselov <ilyaveselov05@yandex.ru>
2025-05-29 13:03:21 +0400

    Добавил пакет nLog, зарегистрировал его в Program.cs, добавил конфиг для логгера

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,3 +1,4 @@
+using NLog.Web;
 using Task2.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
+
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
 
 var app = builder.Build();
 
