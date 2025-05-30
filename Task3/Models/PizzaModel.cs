936de5abff105334746df34adc5ba7a26d725c5d
Ilya Veselov <ilyaveselov05@yandex.ru>
2025-05-30 09:14:42 +0400

    Инициализировал новое решение, вернул Details и CardModal (для перехода на новую страницу подробного просмотра с представления Index при нажатии на картинку или название пиццы

---
+++ Task3/Models/PizzaModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task3.Models
+{
+    public class PizzaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Пицца обязательно должна иметь название")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Image { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Пицца обязательно должна иметь описание")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Укажите размеры")]
+        [MinLength(1, ErrorMessage = "Должен быть хотя бы один размер")]
+        public List<int> Sizes { get; set; } = new();
+        public List<string> Types { get; set; } = new();
+
+        [Required(ErrorMessage = "Пицца обязательно должна иметь цену")]
+        [Range(100, int.MaxValue, ErrorMessage = "Цена должна быть от 100 руб.")]
+        public int Price { get; set; }
+
+        public int Weight { get; set; } = 100;
+        public bool ShowHalf { get; set; } = false;
+        public bool CanHalf { get; set; } = false;
+        public bool IsHit { get; set; } = false;
+    }
+}
---
+++ Task3/Repositories/IPizzaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Task3.Models;
+
+namespace Task3.Repositories
+{
+    public interface IPizzaRepository
+    {
+        List<PizzaModel> GetAllPizzas();
+
+        PizzaModel GetPizzaById(int id);
+
+        void CreatePizza(PizzaModel pizza);
+    }
+}
